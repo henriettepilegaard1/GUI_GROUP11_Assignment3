98543e848bd69e98ced2d86ccaf89d138f178a07
carolinekdl <43413734+carolinekdl@users.noreply.github.com>
2020-05-13 21:01:32 +0200

    navbar

--- ModelsApi/VueModels/VueModels/src/main.js
+++ ModelsApi/VueModels/VueModels/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router'
+import vueResource from 'vue-resource'
 
 Vue.config.productionTip = true;
 Vue.use(VueRouter)
+Vue.use(vueResource)
 
 const router = new VueRouter({
     mode: 'history',
@@ -19,8 +21,27 @@ new Vue({
     router,
     template: `
         <div id="app>
-            <router-link to="/managers">Managers</router-link>
-            <router-link to="/jobs">Jobs</router-link>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div-class="navbar-header">
+                    <button type="button" class="navbar-toogle collapsed" data-toggle="collapse" data-taget="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation </span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Models Management</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><router-link to="/">Models</router-link></li>
+                        <li><router-link to="/managers">Managers</router-link></li>
+                        <li><router-link to="/jobs">Jobs</router-link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            <router-view></router-view> 
         </div>
     `
 }).$mount('#app');

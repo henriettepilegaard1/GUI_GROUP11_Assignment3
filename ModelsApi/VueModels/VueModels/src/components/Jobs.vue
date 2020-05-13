187ab27f26cc60081bb98edae139258b9fbbee47
carolinekdl <43413734+carolinekdl@users.noreply.github.com>
2020-05-13 20:21:42 +0200

    add components and router

---
+++ ModelsApi/VueModels/VueModels/src/components/Jobs.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="jobs">
+        JOBS
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Jobs',
+        data () {
+            return {
+
+            }
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    
+</style>
--- ModelsApi/VueModels/VueModels/src/main.js
+++ ModelsApi/VueModels/VueModels/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = true;
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {path: '/', component: Models},
+        {path: '/managers', component: Managers},
+        {path: '/jobs', compnent: Jobs}
+    ]
+})
 
 new Vue({
-    render: h => h(App)
+    router,
+    template: `
+        <div id="app>
+            <router-link to="/managers">Managers</router-link>
+            <router-link to="/jobs">Jobs</router-link>
+        </div>
+    `
 }).$mount('#app');

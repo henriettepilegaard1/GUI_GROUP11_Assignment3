bc0f2de0af4eefd392c9f85c84810a6b80e185d7
carolinekdl <43413734+carolinekdl@users.noreply.github.com>
2020-05-17 15:56:10 +0200

    add manager

--- vuetest2/src/components/Managers.vue
+++ vuetest2/src/components/Managers.vue
@@ -4,29 +4,77 @@
     <div class="clearfix">
         <h3 class="page-header" style="float: left;">Managers</h3>
     </div>
-    <div>
-        <h7>On this page you can..</h7>
-    </div>
     <br>
+    <div>
+        <p>On this page you can add new managers by filling in the required fields.</p>
+    </div>
+    <hr class="mb-4">
+    <br>
+          <form v-on:submit="addManager" @submit.prevent="false" class="needs-validation" novalidate>
+        <div class="row">
+          <div class="col-md-6 mb-3">
+            <label for="firstName">First name*</label>
+            <input type="text" class="form-control" id="firstName" placeholder="" value="" required >
+            <div class="invalid-feedback">
+              Valid first name is required.
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-3">
+            <label for="lastName">Last name*</label>
+            <input type="text" class="form-control" id="lastName" placeholder="" value="" required >
+            <div class="invalid-feedback">
+              Valid last name is required.
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 mb-3">
+            <label for="email">E-mail*</label>
+            <input type="email" class="form-control" id="email" placeholder="" value="" required >
+            <div class="invalid-feedback">
+              Valid e-mail is required.
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-3">
+            <label for="password">Password*</label>
+            <input type="password" class="form-control" id="password" placeholder="" value="" required >
+            <div class="invalid-feedback">
+              Valid password is required.
+            </div>
+          </div>
+        </div>
+        
+        <hr class="mb-4">
+
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
+        <br>
+        <br>
+        <br>
+      </form>
     </div>
 </template>
 
 <script>
-//import AppNav from './components/AppNav';
-
     export default {
-        name: 'managers',
-        components: {
-            //AppNav
-        },
+        name: 'addManager',
         data() {
             return {
+                manager: {}
+            };
+        },
 
-            }
-        }
     };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+*{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+        .routerLink{
+        text-decoration: none;
+    }
 </style>

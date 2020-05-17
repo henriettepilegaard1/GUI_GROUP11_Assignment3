2ed1afd574593029429738ec74bfcf177264fa2a
carolinekdl <43413734+carolinekdl@users.noreply.github.com>
2020-05-17 17:18:17 +0200

    List of jobs + add new job

---
+++ vuetest2/src/components/AddJob.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="addJob container">
+    <br>
+    <div class="clearfix">
+        <h3 class="page-header" style="float: left;">Add new job</h3>
+    </div>
+    <br>
+    <div>
+        <p>On this page you can add a new job by filling in the required fields.</p>
+    </div>
+    <hr class="mb-4">
+
+      <form v-on:submit="addJob" @submit.prevent="false" class="needs-validation" validate>
+        <div class="mb-3">
+          <label for="customer">Customer</label>
+          <input type="customer" class="form-control" id="customer" placeholder="" required v-model="job.costumer">
+          <div class="invalid-feedback">
+              Please enter a customer.
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="col md6 mb-3">
+          <label for="date">Start Date</label>
+          <input type="date" class="form-control" id="startDate" placeholder="" v-model="job.startDate">
+          <div class="invalid-feedback">
+              Please choose a start date.
+            </div>
+        </div>
+
+        <div class="col md-6 mb-3">
+          <label for="days">Days*</label>
+          <input type="days" class="form-control" id="days" placeholder="Amount of days" v-model="job.days">
+          <div class="invalid-feedback">
+              Please enter amount of days.
+            </div>
+        </div>
+        
+    </div>
+    <div class="mb-3">
+          <label for="location">Location*</label>
+          <input type="location" class="form-control" id="location" placeholder="" required v-model="job.location">
+          <div class="invalid-feedback">
+              Please enter a location.
+            </div>
+        </div>
+
+     <div class="mb-3">
+          <label for="comments">Comments <span class="text-muted">(Optional)</span></label>
+          <input type="multiLine" class="form-control" id="comments" placeholder="">
+        </div>
+    <br>
+        <hr class="mb-4">
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
+      </form>
+      <br>
+      <br>
+      <br>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+export default {
+  name: "addJob",
+  data() {
+    return {
+        job: {}
+    };
+  },
+  methods: {
+    addJob: function() {
+      axios.post(`https://localhost:44368/api/account/login`, {
+            costumer: this.costumer,
+            startDate: this.startDate,
+            days: this.days,
+            location: this.location,
+            comments: this.comments
+        }, 
+        {
+            headers: { 
+            'Content-Type': 'application/json'
+            }
+        }
+    )
+    .then(response => {
+        console.log(job)
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+    }
+  }
+};
+    
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    *{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+        .routerLink{
+        text-decoration: none;
+    }
+    
+</style>
--- vuetest2/src/components/Jobs.vue
+++ vuetest2/src/components/Jobs.vue
@@ -1,27 +1,73 @@
 <template>
     <div class="jobs container">
+    <br>
+    <div class="clearfix">
+        <h3 class="page-header" style="float: left;">Jobs</h3>
+    </div>
+    <div>
         <br>
-        <div class="clearfix">
-            <h3 class="page-header" style="float: left;">Jobs</h3>
-        </div>
-        <div>
-            <h7>On this page you can..</h7>
-        </div>
-        <br>
+        <p>On this page you will see a list of your jobs if you are a model. If you are a manager, you will see a complete list of all jobs. You can click any job to show further details and what models are assigned to the job.</p>
+    </div>
+    <br>
+        <table class="table table-striped">
+            <thead> 
+                <tr>
+                    <th bgcolor="#fff">Customer</th>
+                    <th bgcolor="#fff">Start Date</th>
+                    <th bgcolor="#fff">Days</th>
+                    <th bgcolor="#fff">Location</th>
+                </tr>
+            </thead>
+            <tr><td>&nbsp;</td></tr>
+            <tbody>
+                <!-- <tr v-for="job in jobs">
+                    <td>{{job.customer}}</td>
+                    <td>{{job.startDate}}</td>
+                    <td>{{job.days}}</td>
+                    <td>{{job.location}}</td> 
+                </tr>-->
+            </tbody>
+        </table>
+        <button class="btn btn-primary" style="float: right; position: relative; top: 10px;">
+            <router-link style="text-decoration: none; color: inherit;" to="/AddJob">Add new job</router-link> <!-- Remove html blue link color -->
+        </button>
     </div>
 </template>
 
 <script>
+//import axios from 'axios';
+import Header from './Header';
+
     export default {
         name: 'jobs',
+        components: {
+            Header
+        },
         data() {
             return {
-
+                models: []
             }
+        },
+        methods: {
+            fetchJobs(){
+                axois.get('/api/Jobs')
+                    .then(function(response){
+                        this.models = JSON.parse(response.body)
+                    });
+            }
+        },
+        created: function(){
+            this.fetchJobs
         }
     };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+*{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+    .routerLink{
+     text-decoration: none;
+ }
 </style>
